pejaycz/shopsys

admin: added site content and email template for personal data export

packages/framework/src/Form/Admin/PersonalData/PersonalDataFormType.php
@@ -12,9 +12,13 @@ class PersonalDataFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', CKEditorType::class, [
+            ->add('contentDisplayPerosnalData', CKEditorType::class, [
                 'required' => false,
             ])
+            ->add('contentExportPersonalData', CKEditorType::class, [
+                'required' => false,
+
+                ])
             ->add('save', SubmitType::class);
     }
 }

shopsys/framework

src/Form/Admin/PersonalData/PersonalDataFormType.php
@@ -12,9 +12,13 @@ class PersonalDataFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', CKEditorType::class, [
+            ->add('personalDataDisplaySiteContent', CKEditorType::class, [
                 'required' => false,
             ])
+            ->add('personalDataExportSiteContent', CKEditorType::class, [
+                'required' => false,
+
+                ])
             ->add('save', SubmitType::class);
     }
 }
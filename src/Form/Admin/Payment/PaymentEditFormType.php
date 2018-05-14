shopsys/shopsys

ImageUploadType: created for simple usage of file upload widget with image preview

packages/framework/src/Form/Admin/Payment/PaymentEditFormType.php
@@ -2,6 +2,7 @@
 
 namespace Shopsys\FrameworkBundle\Form\Admin\Payment;
 
+use Shopsys\FrameworkBundle\Model\Payment\Detail\PaymentDetail;
 use Shopsys\FrameworkBundle\Model\Payment\PaymentEditData;
 use Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade;
 use Symfony\Component\Form\AbstractType;
@@ -29,8 +30,13 @@ class PaymentEditFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $paymentDetail = $options['payment_detail'];
+        /* @var $paymentDetail \Shopsys\FrameworkBundle\Model\Payment\Detail\PaymentDetail */
+
         $builder
-            ->add('paymentData', PaymentFormType::class)
+            ->add('paymentData', PaymentFormType::class, [
+                'payment' => $paymentDetail !== null ? $paymentDetail->getPayment() : null,
+            ])
             ->add($this->getPricesBuilder($builder))
             ->add('save', SubmitType::class);
     }
@@ -70,9 +76,11 @@ class PaymentEditFormType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => PaymentEditData::class,
-            'attr' => ['novalidate' => 'novalidate'],
-        ]);
+        $resolver->setRequired('payment_detail')
+            ->addAllowedTypes('payment_detail', [PaymentDetail::class, 'null'])
+            ->setDefaults([
+                'data_class' => PaymentEditData::class,
+                'attr' => ['novalidate' => 'novalidate'],
+            ]);
     }
 }

packages/framework/src/Form/Admin/Transport/TransportEditFormType.php
@@ -3,6 +3,7 @@
 namespace Shopsys\FrameworkBundle\Form\Admin\Transport;
 
 use Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade;
+use Shopsys\FrameworkBundle\Model\Transport\Detail\TransportDetail;
 use Shopsys\FrameworkBundle\Model\Transport\TransportEditData;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
@@ -29,8 +30,13 @@ class TransportEditFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $transportDetail = $options['transport_detail'];
+        /* @var $transportDetail \Shopsys\FrameworkBundle\Model\Transport\Detail\TransportDetail */
+
         $builder
-            ->add('transportData', TransportFormType::class)
+            ->add('transportData', TransportFormType::class, [
+                'transport' => $transportDetail !== null ? $transportDetail->getTransport() : null,
+            ])
             ->add($this->getPricesBuilder($builder))
             ->add('save', SubmitType::class);
     }
@@ -70,9 +76,12 @@ class TransportEditFormType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => TransportEditData::class,
-            'attr' => ['novalidate' => 'novalidate'],
-        ]);
+        $resolver->setRequired('transport_detail')
+            ->addAllowedTypes('transport_detail', [TransportDetail::class, 'null'])
+            ->setDefaults([
+                'data_class' => TransportEditData::class,
+                'attr' => ['novalidate' => 'novalidate'],
+                'base_prices_by_currency_id' => [],
+            ]);
     }
 }

shopsys/framework

src/Form/Admin/Payment/PaymentEditFormType.php
@@ -2,6 +2,7 @@
 
 namespace Shopsys\FrameworkBundle\Form\Admin\Payment;
 
+use Shopsys\FrameworkBundle\Model\Payment\Detail\PaymentDetail;
 use Shopsys\FrameworkBundle\Model\Payment\PaymentEditData;
 use Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade;
 use Symfony\Component\Form\AbstractType;
@@ -29,8 +30,13 @@ class PaymentEditFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $paymentDetail = $options['payment_detail'];
+        /* @var $paymentDetail \Shopsys\FrameworkBundle\Model\Payment\Detail\PaymentDetail */
+
         $builder
-            ->add('paymentData', PaymentFormType::class)
+            ->add('paymentData', PaymentFormType::class, [
+                'payment' => $paymentDetail !== null ? $paymentDetail->getPayment() : null,
+            ])
             ->add($this->getPricesBuilder($builder))
             ->add('save', SubmitType::class);
     }
@@ -70,9 +76,11 @@ class PaymentEditFormType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => PaymentEditData::class,
-            'attr' => ['novalidate' => 'novalidate'],
-        ]);
+        $resolver->setRequired('payment_detail')
+            ->setAllowedTypes('payment_detail', [PaymentDetail::class, 'null'])
+            ->setDefaults([
+                'data_class' => PaymentEditData::class,
+                'attr' => ['novalidate' => 'novalidate'],
+            ]);
     }
 }

src/Form/Admin/Transport/TransportEditFormType.php
@@ -3,6 +3,7 @@
 namespace Shopsys\FrameworkBundle\Form\Admin\Transport;
 
 use Shopsys\FrameworkBundle\Model\Pricing\Currency\CurrencyFacade;
+use Shopsys\FrameworkBundle\Model\Transport\Detail\TransportDetail;
 use Shopsys\FrameworkBundle\Model\Transport\TransportEditData;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
@@ -29,8 +30,13 @@ class TransportEditFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $transportDetail = $options['transport_detail'];
+        /* @var $transportDetail \Shopsys\FrameworkBundle\Model\Transport\Detail\TransportDetail */
+
         $builder
-            ->add('transportData', TransportFormType::class)
+            ->add('transportData', TransportFormType::class, [
+                'transport' => $transportDetail !== null ? $transportDetail->getTransport() : null,
+            ])
             ->add($this->getPricesBuilder($builder))
             ->add('save', SubmitType::class);
     }
@@ -70,9 +76,11 @@ class TransportEditFormType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => TransportEditData::class,
-            'attr' => ['novalidate' => 'novalidate'],
-        ]);
+        $resolver->setRequired('transport_detail')
+            ->setAllowedTypes('transport_detail', [TransportDetail::class, 'null'])
+            ->setDefaults([
+                'data_class' => TransportEditData::class,
+                'attr' => ['novalidate' => 'novalidate'],
+            ]);
     }
 }
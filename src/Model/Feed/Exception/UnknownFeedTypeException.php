83b2ed13b57cfecd2cff7e5b1620003c628ff4e5
Petr Heinz <petr.heinz@shopsys.com>
2018-06-22 14:26:54 +0200

    complete refactoring of feeds functionality
    - modules are responsible for querying the data to improve performance
    - interfaces from package product-feed-interface are not used anymore as they were only important with open-box architecture
    - only relevant data is fetched from the database, should result in enhanced performance
    - FeedInterface and FeedInfoInterface define the way feeds are registered in the system
    - FeedExport is responsible for the actual generation of a file in batches on a specific domain
    - FeedRenderer is responsible for rendering the feed from Twig template
    - FeedPathProvider is responsible for providing the correct filepath and url to the specified feed on a domain
    - ProductUrlsBatchLoader and ProductParametersBatchLoader are responsible for loading product data in batches
    - cron modules use the logger for debug information
    - DailyFeedCronModule is responsible for continuation of the correct feed after waking up

--- src/Model/Feed/Exception/UnknownFeedTypeException.php
+++ src/Model/Feed/Exception/UnknownFeedTypeException.php
@@ -4,17 +4,17 @@ namespace Shopsys\FrameworkBundle\Model\Feed\Exception;
 
 use Exception;
 
-class UnknownFeedConfigTypeException extends Exception implements FeedException
+class UnknownFeedTypeException extends Exception implements FeedException
 {
     /**
-     * @param int $type
+     * @param string $type
      * @param string[] $knownTypes
      * @param \Exception|null $previous
      */
-    public function __construct($type, array $knownTypes, Exception $previous = null)
+    public function __construct(string $type, array $knownTypes, Exception $previous = null)
     {
         $message = sprintf(
-            'Trying to register feed config of an unknown type "%s". Allowed types are: %s.',
+            'Trying to register or access a feed of an unknown type "%s". Allowed types are: %s.',
             $type,
             implode(', ', $knownTypes)
         );

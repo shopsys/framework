simplybusiness/shopsys

refactored UserDataFactory into interface and standardized creating from User

packages/framework/src/Controller/Admin/CustomerController.php
@@ -20,7 +20,7 @@ use Shopsys\FrameworkBundle\Model\Customer\CustomerDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerFacade;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerListAdminFacade;
 use Shopsys\FrameworkBundle\Model\Customer\User;
-use Shopsys\FrameworkBundle\Model\Customer\UserDataFactory;
+use Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Order\OrderFacade;
 use Shopsys\FrameworkBundle\Model\Security\LoginAsUserFacade;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,7 +31,7 @@ class CustomerController extends AdminBaseController
     const LOGIN_AS_TOKEN_ID_PREFIX = 'loginAs';
 
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory
+     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface
      */
     private $userDataFactory;
 
@@ -86,7 +86,7 @@ class CustomerController extends AdminBaseController
     private $customerDataFactory;
 
     public function __construct(
-        UserDataFactory $userDataFactory,
+        UserDataFactoryInterface $userDataFactory,
         CustomerListAdminFacade $customerListAdminFacade,
         CustomerFacade $customerFacade,
         Breadcrumb $breadcrumb,
@@ -214,7 +214,7 @@ class CustomerController extends AdminBaseController
     {
         $customerData = $this->customerDataFactory->create();
         $selectedDomainId = $this->adminDomainTabsFacade->getSelectedDomainId();
-        $userData = $this->userDataFactory->createDefault($selectedDomainId);
+        $userData = $this->userDataFactory->createFromDomainId($selectedDomainId);
         $customerData->userData = $userData;
 
         $form = $this->createForm(CustomerFormType::class, $customerData, [

packages/framework/src/DataFixtures/Demo/UserDataFixtureLoader.php
@@ -8,7 +8,7 @@ use Shopsys\FrameworkBundle\Component\String\TransformString;
 use Shopsys\FrameworkBundle\Model\Customer\BillingAddressDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\DeliveryAddressDataFactoryInterface;
-use Shopsys\FrameworkBundle\Model\Customer\UserDataFactory;
+use Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface;
 
 class UserDataFixtureLoader
 {
@@ -47,7 +47,7 @@ class UserDataFixtureLoader
     private $path;
 
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory
+     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface
      */
     private $userDataFactory;
 
@@ -74,7 +74,7 @@ class UserDataFixtureLoader
     /**
      * @param string $path
      * @param \Shopsys\FrameworkBundle\Component\Csv\CsvReader $csvReader
-     * @param \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory $userDataFactory
+     * @param \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface $userDataFactory
      * @param \Shopsys\FrameworkBundle\Model\Customer\CustomerDataFactoryInterface $customerDataFactory
      * @param \Shopsys\FrameworkBundle\Model\Customer\BillingAddressDataFactoryInterface $billingAddressDataFactory
      * @param \Shopsys\FrameworkBundle\Model\Customer\DeliveryAddressDataFactoryInterface $deliveryAddressDataFactory
@@ -82,7 +82,7 @@ class UserDataFixtureLoader
     public function __construct(
         $path,
         CsvReader $csvReader,
-        UserDataFactory $userDataFactory,
+        UserDataFactoryInterface $userDataFactory,
         CustomerDataFactoryInterface $customerDataFactory,
         BillingAddressDataFactoryInterface $billingAddressDataFactory,
         DeliveryAddressDataFactoryInterface $deliveryAddressDataFactory
@@ -156,7 +156,7 @@ class UserDataFixtureLoader
     {
         $customerData = $this->customerDataFactory->create();
         $domainId = (int)$row[self::COLUMN_DOMAIN_ID];
-        $userData = $this->userDataFactory->createDefault($domainId);
+        $userData = $this->userDataFactory->createFromDomainId($domainId);
         $billingAddressData = $this->billingAddressDataFactory->create();
 
         $userData->firstName = $row[self::COLUMN_FIRSTNAME];

packages/framework/src/DataFixtures/Performance/UserDataFixture.php
@@ -13,7 +13,7 @@ use Shopsys\FrameworkBundle\Model\Customer\BillingAddressDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerFacade;
 use Shopsys\FrameworkBundle\Model\Customer\DeliveryAddressDataFactoryInterface;
-use Shopsys\FrameworkBundle\Model\Customer\UserDataFactory;
+use Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class UserDataFixture
@@ -46,7 +46,7 @@ class UserDataFixture
     private $customerEditFacade;
 
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory
+     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface
      */
     private $userDataFactory;
 
@@ -86,7 +86,7 @@ class UserDataFixture
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      * @param \Shopsys\FrameworkBundle\Component\Doctrine\SqlLoggerFacade $sqlLoggerFacade
      * @param \Shopsys\FrameworkBundle\Model\Customer\CustomerFacade $customerEditFacade
-     * @param \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory $userDataFactory
+     * @param \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface $userDataFactory
      * @param \Faker\Generator $faker
      * @param \Shopsys\FrameworkBundle\Component\DataFixture\PersistentReferenceFacade $persistentReferenceFacade
      * @param \Shopsys\FrameworkBundle\Component\Console\ProgressBarFactory $progressBarFactory
@@ -100,7 +100,7 @@ class UserDataFixture
         Domain $domain,
         SqlLoggerFacade $sqlLoggerFacade,
         CustomerFacade $customerEditFacade,
-        UserDataFactory $userDataFactory,
+        UserDataFactoryInterface $userDataFactory,
         Faker $faker,
         PersistentReferenceFacade $persistentReferenceFacade,
         ProgressBarFactory $progressBarFactory,
@@ -175,7 +175,7 @@ class UserDataFixture
 
         $country = $this->persistentReferenceFacade->getReference(CountryDataFixture::COUNTRY_CZECH_REPUBLIC_1);
 
-        $userData = $this->userDataFactory->createDefault($domainId);
+        $userData = $this->userDataFactory->createFromDomainId($domainId);
         $userData->firstName = $this->faker->firstName;
         $userData->lastName = $this->faker->lastName;
         $userData->email = $userNumber . '.' . $this->faker->safeEmail;

packages/framework/src/Model/Customer/UserDataFactory.php
@@ -4,7 +4,7 @@ namespace Shopsys\FrameworkBundle\Model\Customer;
 
 use Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroupSettingFacade;
 
-class UserDataFactory
+class UserDataFactory implements UserDataFactoryInterface
 {
     /**
      * @var \Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroupSettingFacade
@@ -20,14 +20,57 @@ class UserDataFactory
     }
 
     /**
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function create(): UserData
+    {
+        return new UserData();
+    }
+
+    /**
      * @param int $domainId
      * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
      */
-    public function createDefault($domainId)
+    public function createFromDomainId(int $domainId): UserData
     {
         $userData = new UserData();
-        $userData->pricingGroup = $this->pricingGroupSettingFacade->getDefaultPricingGroupByDomainId($domainId);
+        $this->fillFromDomainId($userData, $domainId);
 
         return $userData;
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Customer\UserData $userData
+     * @param int $domainId
+     */
+    protected function fillFromDomainId(UserData $userData, int $domainId)
+    {
+        $userData->pricingGroup = $this->pricingGroupSettingFacade->getDefaultPricingGroupByDomainId($domainId);
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User $user
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function createFromUser(User $user): UserData
+    {
+        $userData = new UserData();
+        $this->fillFromUser($userData, $user);
+
+        return $userData;
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Customer\UserData $userData
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User $user
+     */
+    private function fillFromUser(UserData $userData, User $user)
+    {
+        $userData->domainId = $user->getDomainId();
+        $userData->firstName = $user->getFirstName();
+        $userData->lastName = $user->getLastName();
+        $userData->email = $user->getEmail();
+        $userData->pricingGroup = $user->getPricingGroup();
+        $userData->createdAt = $user->getCreatedAt();
     }
 }

packages/framework/src/Model/Customer/UserDataFactoryInterface.php
@@ -1,0 +1,23 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Customer;
+
+interface UserDataFactoryInterface
+{
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function create(): UserData;
+
+    /**
+     * @param int $domainId
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function createFromDomainId(int $domainId): UserData;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User $user
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function createFromUser(User $user): UserData;
+}

shopsys/framework

src/Controller/Admin/CustomerController.php
@@ -19,7 +19,7 @@ use Shopsys\FrameworkBundle\Model\Customer\CustomerDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerFacade;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerListAdminFacade;
 use Shopsys\FrameworkBundle\Model\Customer\User;
-use Shopsys\FrameworkBundle\Model\Customer\UserDataFactory;
+use Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Order\OrderFacade;
 use Shopsys\FrameworkBundle\Model\Security\LoginAsUserFacade;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,7 +30,7 @@ class CustomerController extends AdminBaseController
     const LOGIN_AS_TOKEN_ID_PREFIX = 'loginAs';
 
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory
+     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface
      */
     private $userDataFactory;
 
@@ -85,7 +85,7 @@ class CustomerController extends AdminBaseController
     private $customerDataFactory;
 
     public function __construct(
-        UserDataFactory $userDataFactory,
+        UserDataFactoryInterface $userDataFactory,
         CustomerListAdminFacade $customerListAdminFacade,
         CustomerFacade $customerFacade,
         Breadcrumb $breadcrumb,
@@ -213,7 +213,7 @@ class CustomerController extends AdminBaseController
     {
         $customerData = $this->customerDataFactory->create();
         $selectedDomainId = $this->adminDomainTabsFacade->getSelectedDomainId();
-        $userData = $this->userDataFactory->createDefault($selectedDomainId);
+        $userData = $this->userDataFactory->createForDomainId($selectedDomainId);
         $customerData->userData = $userData;
 
         $form = $this->createForm(CustomerFormType::class, $customerData, [

src/DataFixtures/Demo/UserDataFixtureLoader.php
@@ -8,7 +8,7 @@ use Shopsys\FrameworkBundle\Component\String\TransformString;
 use Shopsys\FrameworkBundle\Model\Customer\BillingAddressDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\DeliveryAddressDataFactoryInterface;
-use Shopsys\FrameworkBundle\Model\Customer\UserDataFactory;
+use Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface;
 
 class UserDataFixtureLoader
 {
@@ -47,7 +47,7 @@ class UserDataFixtureLoader
     private $path;
 
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory
+     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface
      */
     private $userDataFactory;
 
@@ -74,7 +74,7 @@ class UserDataFixtureLoader
     /**
      * @param string $path
      * @param \Shopsys\FrameworkBundle\Component\Csv\CsvReader $csvReader
-     * @param \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory $userDataFactory
+     * @param \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface $userDataFactory
      * @param \Shopsys\FrameworkBundle\Model\Customer\CustomerDataFactoryInterface $customerDataFactory
      * @param \Shopsys\FrameworkBundle\Model\Customer\BillingAddressDataFactoryInterface $billingAddressDataFactory
      * @param \Shopsys\FrameworkBundle\Model\Customer\DeliveryAddressDataFactoryInterface $deliveryAddressDataFactory
@@ -82,7 +82,7 @@ class UserDataFixtureLoader
     public function __construct(
         $path,
         CsvReader $csvReader,
-        UserDataFactory $userDataFactory,
+        UserDataFactoryInterface $userDataFactory,
         CustomerDataFactoryInterface $customerDataFactory,
         BillingAddressDataFactoryInterface $billingAddressDataFactory,
         DeliveryAddressDataFactoryInterface $deliveryAddressDataFactory
@@ -156,7 +156,7 @@ class UserDataFixtureLoader
     {
         $customerData = $this->customerDataFactory->create();
         $domainId = (int)$row[self::COLUMN_DOMAIN_ID];
-        $userData = $this->userDataFactory->createDefault($domainId);
+        $userData = $this->userDataFactory->createForDomainId($domainId);
         $billingAddressData = $this->billingAddressDataFactory->create();
 
         $userData->firstName = $row[self::COLUMN_FIRSTNAME];

src/DataFixtures/Performance/UserDataFixture.php
@@ -13,7 +13,7 @@ use Shopsys\FrameworkBundle\Model\Customer\BillingAddressDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerDataFactoryInterface;
 use Shopsys\FrameworkBundle\Model\Customer\CustomerFacade;
 use Shopsys\FrameworkBundle\Model\Customer\DeliveryAddressDataFactoryInterface;
-use Shopsys\FrameworkBundle\Model\Customer\UserDataFactory;
+use Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class UserDataFixture
@@ -46,7 +46,7 @@ class UserDataFixture
     private $customerEditFacade;
 
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory
+     * @var \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface
      */
     private $userDataFactory;
 
@@ -86,7 +86,7 @@ class UserDataFixture
      * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
      * @param \Shopsys\FrameworkBundle\Component\Doctrine\SqlLoggerFacade $sqlLoggerFacade
      * @param \Shopsys\FrameworkBundle\Model\Customer\CustomerFacade $customerEditFacade
-     * @param \Shopsys\FrameworkBundle\Model\Customer\UserDataFactory $userDataFactory
+     * @param \Shopsys\FrameworkBundle\Model\Customer\UserDataFactoryInterface $userDataFactory
      * @param \Faker\Generator $faker
      * @param \Shopsys\FrameworkBundle\Component\DataFixture\PersistentReferenceFacade $persistentReferenceFacade
      * @param \Shopsys\FrameworkBundle\Component\Console\ProgressBarFactory $progressBarFactory
@@ -100,7 +100,7 @@ class UserDataFixture
         Domain $domain,
         SqlLoggerFacade $sqlLoggerFacade,
         CustomerFacade $customerEditFacade,
-        UserDataFactory $userDataFactory,
+        UserDataFactoryInterface $userDataFactory,
         Faker $faker,
         PersistentReferenceFacade $persistentReferenceFacade,
         ProgressBarFactory $progressBarFactory,
@@ -175,7 +175,7 @@ class UserDataFixture
 
         $country = $this->persistentReferenceFacade->getReference(CountryDataFixture::COUNTRY_CZECH_REPUBLIC_1);
 
-        $userData = $this->userDataFactory->createDefault($domainId);
+        $userData = $this->userDataFactory->createForDomainId($domainId);
         $userData->firstName = $this->faker->firstName;
         $userData->lastName = $this->faker->lastName;
         $userData->email = $userNumber . '.' . $this->faker->safeEmail;

src/Model/Customer/UserDataFactory.php
@@ -4,7 +4,7 @@ namespace Shopsys\FrameworkBundle\Model\Customer;
 
 use Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroupSettingFacade;
 
-class UserDataFactory
+class UserDataFactory implements UserDataFactoryInterface
 {
     /**
      * @var \Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroupSettingFacade
@@ -20,14 +20,57 @@ class UserDataFactory
     }
 
     /**
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function create(): UserData
+    {
+        return new UserData();
+    }
+
+    /**
      * @param int $domainId
      * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
      */
-    public function createDefault($domainId)
+    public function createForDomainId(int $domainId): UserData
     {
         $userData = new UserData();
-        $userData->pricingGroup = $this->pricingGroupSettingFacade->getDefaultPricingGroupByDomainId($domainId);
+        $this->fillForDomainId($userData, $domainId);
 
         return $userData;
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Customer\UserData $userData
+     * @param int $domainId
+     */
+    protected function fillForDomainId(UserData $userData, int $domainId)
+    {
+        $userData->pricingGroup = $this->pricingGroupSettingFacade->getDefaultPricingGroupByDomainId($domainId);
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User $user
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function createFromUser(User $user): UserData
+    {
+        $userData = new UserData();
+        $this->fillFromUser($userData, $user);
+
+        return $userData;
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Customer\UserData $userData
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User $user
+     */
+    private function fillFromUser(UserData $userData, User $user)
+    {
+        $userData->domainId = $user->getDomainId();
+        $userData->firstName = $user->getFirstName();
+        $userData->lastName = $user->getLastName();
+        $userData->email = $user->getEmail();
+        $userData->pricingGroup = $user->getPricingGroup();
+        $userData->createdAt = $user->getCreatedAt();
     }
 }

src/Model/Customer/UserDataFactoryInterface.php
@@ -1,0 +1,23 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Customer;
+
+interface UserDataFactoryInterface
+{
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function create(): UserData;
+
+    /**
+     * @param int $domainId
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function createForDomainId(int $domainId): UserData;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Customer\User $user
+     * @return \Shopsys\FrameworkBundle\Model\Customer\UserData
+     */
+    public function createFromUser(User $user): UserData;
+}
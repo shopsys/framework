abecko47/shopsys

prevent elasticsearch migration from failing when any indexes are missing

packages/framework/src/Component/Elasticsearch/IndexRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Component\Elasticsearch;
 
 use Elasticsearch\Client;
+use Shopsys\FrameworkBundle\Component\Elasticsearch\Exception\ElasticsearchIndexAlreadyExists;
 use Shopsys\FrameworkBundle\Component\Elasticsearch\Exception\ElasticsearchIndexException;
 use Shopsys\FrameworkBundle\Component\Elasticsearch\Exception\ElasticsearchNoAliasException;
 
@@ -32,7 +33,7 @@ class IndexRepository
         $indexes = $this->elasticsearchClient->indices();
 
         if ($indexes->exists(['index' => $indexName])) {
-            throw ElasticsearchIndexException::indexAlreadyExists($indexName);
+            throw new ElasticsearchIndexAlreadyExists($indexName);
         }
 
         $result = $indexes->create([

shopsys/framework

src/Component/Elasticsearch/IndexRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shopsys\FrameworkBundle\Component\Elasticsearch;
 
 use Elasticsearch\Client;
+use Shopsys\FrameworkBundle\Component\Elasticsearch\Exception\ElasticsearchIndexAlreadyExistsException;
 use Shopsys\FrameworkBundle\Component\Elasticsearch\Exception\ElasticsearchIndexException;
 use Shopsys\FrameworkBundle\Component\Elasticsearch\Exception\ElasticsearchNoAliasException;
 
@@ -32,7 +33,7 @@ class IndexRepository
         $indexes = $this->elasticsearchClient->indices();
 
         if ($indexes->exists(['index' => $indexName])) {
-            throw ElasticsearchIndexException::indexAlreadyExists($indexName);
+            throw new ElasticsearchIndexAlreadyExistsException($indexName);
         }
 
         $result = $indexes->create([
7ab8bd2b85145f075c4c4aabf492881dd0affd9a
Martin Grossmann <martin@vyvoj.net>
2023-11-11 23:50:54 +0100

    transport daysUntilDelivery is now moved to shopsys/framework

---
+++ src/Migrations/Version20231213165330.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20231213165330 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        if ($this->isAppMigrationNotInstalledRemoveIfExists('Version20200909112133')) {
+            $this->sql('ALTER TABLE transports ADD days_until_delivery INT NOT NULL DEFAULT 0');
+            $this->sql('ALTER TABLE transports ALTER days_until_delivery DROP DEFAULT');
+        }
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}

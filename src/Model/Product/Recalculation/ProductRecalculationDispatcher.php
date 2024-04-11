shopsys/shopsys

string is now used instead of ProductRecalculationPriorityEnum
- it is not possible to extend PHP enums

packages/framework/src/Model/Product/Recalculation/ProductRecalculationDispatcher.php
@@ -11,34 +11,35 @@ class ProductRecalculationDispatcher extends AbstractMessageDispatcher
 {
     /**
      * @param \Shopsys\FrameworkBundle\Model\Product\Product[] $products
-     * @param \Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationPriorityEnum $productRecalculationPriorityEnum
+     * @param string $productRecalculationPriority
      * @return int[]
      */
     public function dispatchProducts(
         array $products,
-        ProductRecalculationPriorityEnumInterface $productRecalculationPriorityEnum = ProductRecalculationPriorityEnum::REGULAR,
+        string $productRecalculationPriority = ProductRecalculationPriority::REGULAR,
     ): array {
         return $this->dispatchProductIds(
             array_map(static fn (Product $product) => $product->getId(), $products),
-            $productRecalculationPriorityEnum,
+            $productRecalculationPriority,
         );
     }
 
     /**
      * @param int[] $productIds
-     * @param \Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationPriorityEnum $productRecalculationPriorityEnum
+     * @param string $productRecalculationPriority
      * @return int[]
      */
     public function dispatchProductIds(
         array $productIds,
-        ProductRecalculationPriorityEnumInterface $productRecalculationPriorityEnum = ProductRecalculationPriorityEnum::REGULAR,
+        string $productRecalculationPriority = ProductRecalculationPriority::REGULAR,
     ): array {
         $productIds = array_unique($productIds);
 
         foreach ($productIds as $productId) {
-            $message = match ($productRecalculationPriorityEnum) {
-                ProductRecalculationPriorityEnum::HIGH => new ProductRecalculationPriorityHighMessage((int)$productId),
-                ProductRecalculationPriorityEnum::REGULAR => new ProductRecalculationPriorityRegularMessage((int)$productId),
+            $message = match ($productRecalculationPriority) {
+                ProductRecalculationPriority::HIGH => new ProductRecalculationPriorityHighMessage((int)$productId),
+                ProductRecalculationPriority::REGULAR => new ProductRecalculationPriorityRegularMessage((int)$productId),
+                default => throw new UnknownProductRecalculationPriorityException($productRecalculationPriority),
             };
             $this->messageBus->dispatch($message);
         }
@@ -48,13 +49,13 @@ class ProductRecalculationDispatcher extends AbstractMessageDispatcher
 
     /**
      * @param int $productId
-     * @param \Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationPriorityEnum $productRecalculationPriorityEnum
+     * @param string $productRecalculationPriority
      */
     public function dispatchSingleProductId(
         int $productId,
-        ProductRecalculationPriorityEnumInterface $productRecalculationPriorityEnum = ProductRecalculationPriorityEnum::REGULAR,
+        string $productRecalculationPriority = ProductRecalculationPriority::REGULAR,
     ): void {
-        $this->dispatchProductIds([$productId], $productRecalculationPriorityEnum);
+        $this->dispatchProductIds([$productId], $productRecalculationPriority);
     }
 
     public function dispatchAllProducts(): void

shopsys/framework

src/Model/Product/Recalculation/ProductRecalculationDispatcher.php
@@ -21,13 +21,13 @@ class ProductRecalculationDispatcher extends AbstractMessageDispatcher
 
     /**
      * @param \Shopsys\FrameworkBundle\Model\Product\Product[] $products
-     * @param \Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationPriorityEnum $productRecalculationPriorityEnum
+     * @param string $productRecalculationPriorityEnum
      * @param string[] $exportScopes
      * @return int[]
      */
     public function dispatchProducts(
         array $products,
-        ProductRecalculationPriorityEnumInterface $productRecalculationPriorityEnum = ProductRecalculationPriorityEnum::REGULAR,
+        string $productRecalculationPriorityEnum = ProductRecalculationPriorityEnum::REGULAR,
         array $exportScopes = [],
     ): array {
         return $this->dispatchProductIds(
@@ -39,13 +39,13 @@ class ProductRecalculationDispatcher extends AbstractMessageDispatcher
 
     /**
      * @param int[] $productIds
-     * @param \Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationPriorityEnum $productRecalculationPriorityEnum
+     * @param string $productRecalculationPriorityEnum
      * @param string[] $exportScopes
      * @return int[]
      */
     public function dispatchProductIds(
         array $productIds,
-        ProductRecalculationPriorityEnumInterface $productRecalculationPriorityEnum = ProductRecalculationPriorityEnum::REGULAR,
+        string $productRecalculationPriorityEnum = ProductRecalculationPriorityEnum::REGULAR,
         array $exportScopes = [],
     ): array {
         $this->verifyExportScopes($exportScopes);
@@ -55,6 +55,7 @@ class ProductRecalculationDispatcher extends AbstractMessageDispatcher
             $message = match ($productRecalculationPriorityEnum) {
                 ProductRecalculationPriorityEnum::HIGH => new ProductRecalculationPriorityHighMessage((int)$productId, $exportScopes),
                 ProductRecalculationPriorityEnum::REGULAR => new ProductRecalculationPriorityRegularMessage((int)$productId, $exportScopes),
+                default => throw new UnknownProductRecalculationPriorityException($productRecalculationPriorityEnum),
             };
             $this->messageBus->dispatch($message);
         }
@@ -64,12 +65,12 @@ class ProductRecalculationDispatcher extends AbstractMessageDispatcher
 
     /**
      * @param int $productId
-     * @param \Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationPriorityEnum $productRecalculationPriorityEnum
+     * @param string $productRecalculationPriorityEnum
      * @param string[] $exportScopes
      */
     public function dispatchSingleProductId(
         int $productId,
-        ProductRecalculationPriorityEnumInterface $productRecalculationPriorityEnum = ProductRecalculationPriorityEnum::REGULAR,
+        string $productRecalculationPriorityEnum = ProductRecalculationPriorityEnum::REGULAR,
         array $exportScopes = [],
     ): void {
         $this->dispatchProductIds([$productId], $productRecalculationPriorityEnum, $exportScopes);
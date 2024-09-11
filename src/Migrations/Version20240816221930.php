shopsys/shopsys

complaint statuses are now administrable

packages/framework/src/Migrations/Version20240816221930.php
@@ -1,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatusEnum;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20240816221930 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $this->sql('CREATE TABLE complaint_statuses (id SERIAL NOT NULL, status VARCHAR(25) NOT NULL, PRIMARY KEY(id))');
+        $this->sql('
+            CREATE TABLE complaint_status_translations (
+                id SERIAL NOT NULL,
+                translatable_id INT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                locale VARCHAR(255) NOT NULL,
+                PRIMARY KEY(id)
+            )');
+        $this->sql('CREATE INDEX IDX_BBBBB6722C2AC5D3 ON complaint_status_translations (translatable_id)');
+        $this->sql('
+            CREATE UNIQUE INDEX complaint_status_translations_uniq_trans ON complaint_status_translations (translatable_id, locale)');
+        $this->sql('
+            ALTER TABLE
+                complaint_status_translations
+            ADD
+                CONSTRAINT FK_BBBBB6722C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES complaint_statuses (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+
+        $this->createComplaintStatusWithEnglishAndCzechTranslations(
+            1,
+            ComplaintStatusEnum::STATUS_NEW,
+            'New',
+            'Nová',
+        );
+        $this->createComplaintStatusWithEnglishAndCzechTranslations(
+            2,
+            'resolved',
+            'Resolved',
+            'Vyřízena',
+        );
+
+        $this->sql('ALTER SEQUENCE complaint_statuses_id_seq RESTART WITH 3');
+    }
+
+    /**
+     * @param int $complaintStatusId
+     * @param string $complaintStatus
+     * @param string $complaintStatusEnglishName
+     * @param string $complaintStatusCzechName
+     */
+    private function createComplaintStatusWithEnglishAndCzechTranslations(
+        int $complaintStatusId,
+        string $complaintStatus,
+        string $complaintStatusEnglishName,
+        string $complaintStatusCzechName,
+    ): void {
+        $this->sql('INSERT INTO complaint_statuses (id, status) VALUES (:id, :status)', [
+            'id' => $complaintStatusId,
+            'status' => $complaintStatus,
+        ]);
+        $this->sql(
+            'INSERT INTO complaint_status_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)',
+            [
+                'translatableId' => $complaintStatusId,
+                'name' => $complaintStatusEnglishName,
+                'locale' => 'en',
+            ],
+        );
+        $this->sql(
+            'INSERT INTO complaint_status_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)',
+            [
+                'translatableId' => $complaintStatusId,
+                'name' => $complaintStatusCzechName,
+                'locale' => 'cs',
+            ],
+        );
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}

packages/framework/src/Model/Complaint/Status/ComplaintStatusFactory.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Complaint\Status;
+
+use Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver;
+
+class ComplaintStatusFactory
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver $entityNameResolver
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatusEnum $complaintStatusEnum
+     */
+    public function __construct(
+        protected readonly EntityNameResolver $entityNameResolver,
+        protected readonly ComplaintStatusEnum $complaintStatusEnum,
+    ) {
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatusData $data
+     * @param string $status
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus
+     */
+    public function create(ComplaintStatusData $data, string $status): ComplaintStatus
+    {
+        $this->complaintStatusEnum->validateCase($status);
+        $entityClassName = $this->entityNameResolver->resolve(ComplaintStatus::class);
+
+        return new $entityClassName($data, $status);
+    }
+}

packages/framework/src/Model/Complaint/Status/ComplaintStatusRepository.php
@@ -1,0 +1,127 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Complaint\Status;
+
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Shopsys\FrameworkBundle\Model\Complaint\Complaint;
+use Shopsys\FrameworkBundle\Model\Complaint\Status\Exception\ComplaintStatusNotFoundException;
+
+class ComplaintStatusRepository
+{
+    protected EntityManagerInterface $em;
+
+    /**
+     * @param \Doctrine\ORM\EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+
+    /**
+     * @return \Doctrine\ORM\EntityRepository
+     */
+    protected function getComplaintStatusRepository(): EntityRepository
+    {
+        return $this->em->getRepository(ComplaintStatus::class);
+    }
+
+    /**
+     * @param int $complaintStatusId
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus|null
+     */
+    public function findById(int $complaintStatusId): ?ComplaintStatus
+    {
+        return $this->getComplaintStatusRepository()->find($complaintStatusId);
+    }
+
+    /**
+     * @param int $complaintStatusId
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus
+     */
+    public function getById(int $complaintStatusId): ComplaintStatus
+    {
+        $complaintStatus = $this->findById($complaintStatusId);
+
+        if ($complaintStatus === null) {
+            $message = 'Complaint status with ID "' . $complaintStatusId . '" not found.';
+
+            throw new ComplaintStatusNotFoundException($message);
+        }
+
+        return $complaintStatus;
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus
+     */
+    public function getDefault(): ComplaintStatus
+    {
+        $complaintStatus = $this->getComplaintStatusRepository()->findOneBy(['status' => ComplaintStatusEnum::STATUS_NEW]);
+
+        if ($complaintStatus === null) {
+            $message = 'Default complaint status not found.';
+
+            throw new ComplaintStatusNotFoundException($message);
+        }
+
+        return $complaintStatus;
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus[]
+     */
+    public function getAll(): array
+    {
+        return $this->getComplaintStatusRepository()->findBy([], ['id' => 'asc']);
+    }
+
+    /**
+     * @param int $complaintStatusId
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus[]
+     */
+    public function getAllExceptId(int $complaintStatusId): array
+    {
+        $qb = $this->getComplaintStatusRepository()->createQueryBuilder('cs')
+            ->where('cs.id != :id')
+            ->setParameter('id', $complaintStatusId);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus $oldComplaintStatus
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus $newComplaintStatus
+     */
+    public function replaceComplaintStatus(
+        ComplaintStatus $oldComplaintStatus,
+        ComplaintStatus $newComplaintStatus,
+    ): void {
+        $this->em->createQueryBuilder()
+            ->update(Complaint::class, 'cmp')
+            ->set('cmp.status', ':newComplaintStatus')->setParameter('newComplaintStatus', $newComplaintStatus)
+            ->where('cmp.status = :oldComplaintStatus')->setParameter('oldComplaintStatus', $oldComplaintStatus)
+            ->getQuery()->execute();
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus $complaintStatus
+     * @return bool
+     */
+    public function isComplaintStatusUsed(ComplaintStatus $complaintStatus): bool
+    {
+        $queryBuilder = $this->em->createQueryBuilder();
+        $queryBuilder
+            ->select('c.id')
+            ->from(Complaint::class, 'c')
+            ->setMaxResults(1)
+            ->where('c.status = :status')
+            ->setParameter('status', $complaintStatus->getId());
+
+        return $queryBuilder->getQuery()->getOneOrNullResult(AbstractQuery::HYDRATE_SCALAR) !== null;
+    }
+}

packages/framework/src/Model/Complaint/Status/Grid/ComplaintStatusGridFactory.php
@@ -1,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Complaint\Status\Grid;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr\Join;
+use Shopsys\FrameworkBundle\Component\Grid\Grid;
+use Shopsys\FrameworkBundle\Component\Grid\GridFactory;
+use Shopsys\FrameworkBundle\Component\Grid\GridFactoryInterface;
+use Shopsys\FrameworkBundle\Component\Grid\QueryBuilderDataSource;
+use Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus;
+use Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatusEnum;
+use Shopsys\FrameworkBundle\Model\Localization\Localization;
+
+class ComplaintStatusGridFactory implements GridFactoryInterface
+{
+    /**
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @param \Shopsys\FrameworkBundle\Component\Grid\GridFactory $gridFactory
+     * @param \Shopsys\FrameworkBundle\Model\Localization\Localization $localization
+     */
+    public function __construct(
+        protected readonly EntityManagerInterface $em,
+        protected readonly GridFactory $gridFactory,
+        protected readonly Localization $localization,
+    ) {
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Component\Grid\Grid
+     */
+    public function create(): Grid
+    {
+        $queryBuilder = $this->em->createQueryBuilder();
+        $queryBuilder
+            ->select('cs, cst')
+            ->from(ComplaintStatus::class, 'cs')
+            ->join('cs.translations', 'cst', Join::WITH, 'cst.locale = :locale')
+            ->setParameter('locale', $this->localization->getAdminLocale());
+        $dataSource = new QueryBuilderDataSource($queryBuilder, 'cs.id');
+
+        $grid = $this->gridFactory->create('complaintStatusList', $dataSource);
+        $grid->setDefaultOrder('name');
+
+        $grid->addColumn('name', 'cst.name', t('Name'), true);
+
+        $grid->setActionColumnClassAttribute('table-col table-col-10');
+        $grid->addDeleteActionColumn('admin_complaintstatus_deleteconfirm', ['id' => 'cs.id'])
+            ->setAjaxConfirm();
+
+        $grid->setTheme('@ShopsysFramework/Admin/Content/ComplaintStatus/listGrid.html.twig', [
+            'STATUS_NEW' => ComplaintStatusEnum::STATUS_NEW,
+            'STATUS_RESOLVED' => ComplaintStatusEnum::STATUS_RESOLVED,
+        ]);
+
+        return $grid;
+    }
+}

shopsys/framework

src/Migrations/Version20240816221930.php
@@ -1,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatusTypeEnum;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20240816221930 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $this->sql('CREATE TABLE complaint_statuses (id SERIAL NOT NULL, status_type VARCHAR(25) NOT NULL, PRIMARY KEY(id))');
+        $this->sql('
+            CREATE TABLE complaint_status_translations (
+                id SERIAL NOT NULL,
+                translatable_id INT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                locale VARCHAR(255) NOT NULL,
+                PRIMARY KEY(id)
+            )');
+        $this->sql('CREATE INDEX IDX_BBBBB6722C2AC5D3 ON complaint_status_translations (translatable_id)');
+        $this->sql('
+            CREATE UNIQUE INDEX complaint_status_translations_uniq_trans ON complaint_status_translations (translatable_id, locale)');
+        $this->sql('
+            ALTER TABLE
+                complaint_status_translations
+            ADD
+                CONSTRAINT FK_BBBBB6722C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES complaint_statuses (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+
+        $this->createComplaintStatusWithEnglishAndCzechTranslations(
+            1,
+            ComplaintStatusTypeEnum::STATUS_TYPE_NEW,
+            'New',
+            'Nová',
+        );
+        $this->createComplaintStatusWithEnglishAndCzechTranslations(
+            2,
+            'resolved',
+            'Resolved',
+            'Vyřízena',
+        );
+
+        $this->sql('ALTER SEQUENCE complaint_statuses_id_seq RESTART WITH 3');
+    }
+
+    /**
+     * @param int $complaintStatusId
+     * @param string $complaintStatus
+     * @param string $complaintStatusEnglishName
+     * @param string $complaintStatusCzechName
+     */
+    private function createComplaintStatusWithEnglishAndCzechTranslations(
+        int $complaintStatusId,
+        string $complaintStatus,
+        string $complaintStatusEnglishName,
+        string $complaintStatusCzechName,
+    ): void {
+        $this->sql('INSERT INTO complaint_statuses (id, status_type) VALUES (:id, :statusType)', [
+            'id' => $complaintStatusId,
+            'statusType' => $complaintStatus,
+        ]);
+        $this->sql(
+            'INSERT INTO complaint_status_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)',
+            [
+                'translatableId' => $complaintStatusId,
+                'name' => $complaintStatusEnglishName,
+                'locale' => 'en',
+            ],
+        );
+        $this->sql(
+            'INSERT INTO complaint_status_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)',
+            [
+                'translatableId' => $complaintStatusId,
+                'name' => $complaintStatusCzechName,
+                'locale' => 'cs',
+            ],
+        );
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}

src/Model/Complaint/Status/ComplaintStatusFactory.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Complaint\Status;
+
+use Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver;
+
+class ComplaintStatusFactory
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver $entityNameResolver
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatusTypeEnum $complaintStatusTypeEnum
+     */
+    public function __construct(
+        protected readonly EntityNameResolver $entityNameResolver,
+        protected readonly ComplaintStatusTypeEnum $complaintStatusTypeEnum,
+    ) {
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatusData $data
+     * @param string $statusType
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus
+     */
+    public function create(ComplaintStatusData $data, string $statusType): ComplaintStatus
+    {
+        $this->complaintStatusTypeEnum->validateCase($statusType);
+        $entityClassName = $this->entityNameResolver->resolve(ComplaintStatus::class);
+
+        return new $entityClassName($data, $statusType);
+    }
+}

src/Model/Complaint/Status/ComplaintStatusRepository.php
@@ -1,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Complaint\Status;
+
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Shopsys\FrameworkBundle\Model\Complaint\Complaint;
+use Shopsys\FrameworkBundle\Model\Complaint\Status\Exception\ComplaintStatusNotFoundException;
+
+class ComplaintStatusRepository
+{
+    /**
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     */
+    public function __construct(
+        protected readonly EntityManagerInterface $em,
+    ) {
+    }
+
+    /**
+     * @return \Doctrine\ORM\EntityRepository
+     */
+    protected function getComplaintStatusRepository(): EntityRepository
+    {
+        return $this->em->getRepository(ComplaintStatus::class);
+    }
+
+    /**
+     * @param int $complaintStatusId
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus|null
+     */
+    public function findById(int $complaintStatusId): ?ComplaintStatus
+    {
+        return $this->getComplaintStatusRepository()->find($complaintStatusId);
+    }
+
+    /**
+     * @param int $complaintStatusId
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus
+     */
+    public function getById(int $complaintStatusId): ComplaintStatus
+    {
+        $complaintStatus = $this->findById($complaintStatusId);
+
+        if ($complaintStatus === null) {
+            $message = 'Complaint status with ID "' . $complaintStatusId . '" not found.';
+
+            throw new ComplaintStatusNotFoundException($message);
+        }
+
+        return $complaintStatus;
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus
+     */
+    public function getDefault(): ComplaintStatus
+    {
+        $complaintStatus = $this->getComplaintStatusRepository()->findOneBy(['statusType' => ComplaintStatusTypeEnum::STATUS_TYPE_NEW]);
+
+        if ($complaintStatus === null) {
+            $message = 'Default complaint status not found.';
+
+            throw new ComplaintStatusNotFoundException($message);
+        }
+
+        return $complaintStatus;
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus[]
+     */
+    public function getAll(): array
+    {
+        return $this->getComplaintStatusRepository()->findBy([], ['id' => 'asc']);
+    }
+
+    /**
+     * @param int $complaintStatusId
+     * @return \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus[]
+     */
+    public function getAllExceptId(int $complaintStatusId): array
+    {
+        $qb = $this->getComplaintStatusRepository()->createQueryBuilder('cs')
+            ->where('cs.id != :id')
+            ->setParameter('id', $complaintStatusId);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus $oldComplaintStatus
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus $newComplaintStatus
+     */
+    public function replaceComplaintStatus(
+        ComplaintStatus $oldComplaintStatus,
+        ComplaintStatus $newComplaintStatus,
+    ): void {
+        $this->em->createQueryBuilder()
+            ->update(Complaint::class, 'cmp')
+            ->set('cmp.status', ':newComplaintStatus')->setParameter('newComplaintStatus', $newComplaintStatus)
+            ->where('cmp.status = :oldComplaintStatus')->setParameter('oldComplaintStatus', $oldComplaintStatus)
+            ->getQuery()->execute();
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus $complaintStatus
+     * @return bool
+     */
+    public function isComplaintStatusUsed(ComplaintStatus $complaintStatus): bool
+    {
+        $queryBuilder = $this->em->createQueryBuilder();
+        $queryBuilder
+            ->select('c.id')
+            ->from(Complaint::class, 'c')
+            ->setMaxResults(1)
+            ->where('c.status = :status')
+            ->setParameter('status', $complaintStatus);
+
+        return $queryBuilder->getQuery()->getOneOrNullResult(AbstractQuery::HYDRATE_SCALAR) !== null;
+    }
+}

src/Model/Complaint/Status/Grid/ComplaintStatusGridFactory.php
@@ -1,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\Complaint\Status\Grid;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr\Join;
+use Shopsys\FrameworkBundle\Component\Grid\Grid;
+use Shopsys\FrameworkBundle\Component\Grid\GridFactory;
+use Shopsys\FrameworkBundle\Component\Grid\GridFactoryInterface;
+use Shopsys\FrameworkBundle\Component\Grid\QueryBuilderDataSource;
+use Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatus;
+use Shopsys\FrameworkBundle\Model\Complaint\Status\ComplaintStatusTypeEnum;
+use Shopsys\FrameworkBundle\Model\Localization\Localization;
+
+class ComplaintStatusGridFactory implements GridFactoryInterface
+{
+    /**
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @param \Shopsys\FrameworkBundle\Component\Grid\GridFactory $gridFactory
+     * @param \Shopsys\FrameworkBundle\Model\Localization\Localization $localization
+     */
+    public function __construct(
+        protected readonly EntityManagerInterface $em,
+        protected readonly GridFactory $gridFactory,
+        protected readonly Localization $localization,
+    ) {
+    }
+
+    /**
+     * @return \Shopsys\FrameworkBundle\Component\Grid\Grid
+     */
+    public function create(): Grid
+    {
+        $queryBuilder = $this->em->createQueryBuilder();
+        $queryBuilder
+            ->select('cs, cst')
+            ->from(ComplaintStatus::class, 'cs')
+            ->join('cs.translations', 'cst', Join::WITH, 'cst.locale = :locale')
+            ->setParameter('locale', $this->localization->getAdminLocale());
+        $dataSource = new QueryBuilderDataSource($queryBuilder, 'cs.id');
+
+        $grid = $this->gridFactory->create('complaintStatusList', $dataSource);
+        $grid->setDefaultOrder('name');
+
+        $grid->addColumn('name', 'cst.name', t('Name'), true);
+
+        $grid->setActionColumnClassAttribute('table-col table-col-10');
+        $grid->addDeleteActionColumn('admin_complaintstatus_deleteconfirm', ['id' => 'cs.id'])
+            ->setAjaxConfirm();
+
+        $grid->setTheme('@ShopsysFramework/Admin/Content/ComplaintStatus/listGrid.html.twig', [
+            'STATUS_TYPE_NEW' => ComplaintStatusTypeEnum::STATUS_TYPE_NEW,
+            'STATUS_TYPE_RESOLVED' => ComplaintStatusTypeEnum::STATUS_TYPE_RESOLVED,
+        ]);
+
+        return $grid;
+    }
+}
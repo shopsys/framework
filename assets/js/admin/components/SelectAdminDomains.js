e7af10251fd3c63c5cd03e62b60c63c492b928e0
Jan Molcik <molcik.jan@gmail.com>
2024-12-03 12:33:51 +0000

    adjust ui of domains filter select  - added custom `checkbox` with indeterminate state instead of select all and select none buttons  - set max height of "Domains filter"and "Go to Frontend" lists and add scrollbar if needed  - moved some styles from `todo.less` to correct file

--- assets/js/admin/components/SelectAdminDomains.js
+++ assets/js/admin/components/SelectAdminDomains.js
@@ -1,27 +1,66 @@
 import Register from '../../common/utils/Register';
 
 export default class SelectAdminDomains {
-
-    constructor ($selectDomainsDropdown) {
+    constructor($selectDomainsDropdown) {
         this.$selectDomainsDropdown = $selectDomainsDropdown;
-        this.$selectDomainsDropdown.find('.js-domains-select-all').on('click', () => this.selectAll());
-        this.$selectDomainsDropdown.find('.js-domains-select-none').on('click', () => this.selectNone());
+        this.$selectDomainsDropdown
+            .find('input[type=checkbox]:not(.js-domains-select-all-checkbox)')
+            .on('change', () => this.updateIndeterminate());
+        this.$selectDomainsDropdown.find('.js-domains-select-all-checkbox').on('click', () => this.checkboxChange());
+        this.updateIndeterminate();
     }
 
-    static init () {
+    static init() {
         $('.js-domains-select').each(function () {
             // eslint-disable-next-line no-new
             new SelectAdminDomains($(this));
         });
     }
 
-    selectAll () {
-        this.$selectDomainsDropdown.find('input[type=checkbox]').prop('checked', true);
+    updateIndeterminate() {
+        let allChecked = true;
+        let allUnchecked = true;
+        this.$selectDomainsDropdown
+            .find('input[type=checkbox]:not(.js-domains-select-all-checkbox)')
+            .each((_, element) => {
+                if ($(element).prop('checked')) {
+                    allUnchecked = false;
+                } else if (!$(element).prop('checked')) {
+                    allChecked = false;
+                }
+            });
+
+        if (allChecked || allUnchecked) {
+            this.$selectDomainsDropdown
+                .find('.js-domains-select-all-checkbox')
+                .prop('indeterminate', false)
+                .prop('checked', allChecked);
+            return;
+        }
+
+        this.$selectDomainsDropdown.find('.js-domains-select-all-checkbox').prop('indeterminate', true);
     }
 
-    selectNone () {
-        this.$selectDomainsDropdown.find('input[type=checkbox]').prop('checked', false);
+    selectAll() {
+        this.$selectDomainsDropdown
+            .find('input[type=checkbox]:not(.js-domains-select-all-checkbox)')
+            .prop('checked', true);
+    }
+
+    selectNone() {
+        this.$selectDomainsDropdown
+            .find('input[type=checkbox]:not(.js-domains-select-all-checkbox)')
+            .prop('checked', false);
+    }
+
+    checkboxChange() {
+        if (!this.$selectDomainsDropdown.find('.js-domains-select-all-checkbox').prop('checked')) {
+            this.selectNone();
+            return;
+        }
+
+        this.selectAll();
     }
 }
 
-(new Register()).registerCallback(SelectAdminDomains.init, 'SelectAdminDomains.init');
+new Register().registerCallback(SelectAdminDomains.init, 'SelectAdminDomains.init');

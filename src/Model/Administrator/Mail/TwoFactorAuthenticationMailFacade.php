liborplucnar/shopsys

ensure manually sent mail templates are wrapped with the GrapesJS body
- MailTemplateFacade::get() renamed to getWrappedWithGrapesBody() for more clarity

packages/framework/src/Model/Administrator/Mail/TwoFactorAuthenticationMailFacade.php
@@ -32,7 +32,7 @@ class TwoFactorAuthenticationMailFacade implements AuthCodeMailerInterface
      */
     public function sendAuthCode(TwoFactorInterface $administrator): void
     {
-        $mailTemplate = $this->mailTemplateFacade->get(
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesBody(
             TwoFactorAuthenticationMail::TWO_FACTOR_AUTHENTICATION_CODE,
             $this->domain->getId(),
         );

packages/framework/src/Model/Complaint/Mail/ComplaintMailFacade.php
@@ -50,7 +50,7 @@ class ComplaintMailFacade
     {
         $templateName = ComplaintMail::getMailTemplateNameByStatus($complaintStatus);
 
-        return $this->mailTemplateFacade->get($templateName, $domainId);
+        return $this->mailTemplateFacade->getWrappedWithGrapesBody($templateName, $domainId);
     }
 
     /**

packages/framework/src/Model/Customer/Mail/CustomerMailFacade.php
@@ -34,7 +34,7 @@ class CustomerMailFacade
      */
     public function sendRegistrationMail(CustomerUser $customerUser): void
     {
-        $mailTemplate = $this->mailTemplateFacade->get(
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesBody(
             MailTemplate::REGISTRATION_CONFIRM_NAME,
             $customerUser->getDomainId(),
         );
@@ -46,7 +46,7 @@ class CustomerMailFacade
      */
     public function sendActivationMail(CustomerUser $customerUser): void
     {
-        $mailTemplate = $this->mailTemplateFacade->get(CustomerActivationMail::CUSTOMER_ACTIVATION_NAME, $customerUser->getDomainId());
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesBody(CustomerActivationMail::CUSTOMER_ACTIVATION_NAME, $customerUser->getDomainId());
         $this->sendActivationMailTemplate($mailTemplate, $customerUser);
     }
 

packages/framework/src/Model/Inquiry/Mail/InquiryMailFacade.php
@@ -32,10 +32,10 @@ class InquiryMailFacade
      */
     public function sendMail(Inquiry $inquiry): void
     {
-        $mailTemplate = $this->mailTemplateFacade->get(InquiryMail::ADMIN_MAIL_TEMPLATE_NAME, $inquiry->getDomainId());
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesBody(InquiryMail::ADMIN_MAIL_TEMPLATE_NAME, $inquiry->getDomainId());
         $this->sendMailTemplate($mailTemplate, $inquiry);
 
-        $mailTemplate = $this->mailTemplateFacade->get(InquiryMail::CUSTOMER_MAIL_TEMPLATE_NAME, $inquiry->getDomainId());
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesBody(InquiryMail::CUSTOMER_MAIL_TEMPLATE_NAME, $inquiry->getDomainId());
         $this->sendMailTemplate($mailTemplate, $inquiry);
     }
 

packages/framework/src/Model/Order/Mail/OrderMailFacade.php
@@ -50,7 +50,7 @@ class OrderMailFacade
     {
         $templateName = OrderMail::getMailTemplateNameByStatus($orderStatus);
 
-        return $this->mailTemplateFacade->get($templateName, $domainId);
+        return $this->mailTemplateFacade->getWrappedWithGrapesBody($templateName, $domainId);
     }
 
     /**

packages/framework/src/Model/PersonalData/Mail/PersonalDataAccessMailFacade.php
@@ -35,12 +35,12 @@ class PersonalDataAccessMailFacade
     public function sendMail(PersonalDataAccessRequest $personalDataAccessRequest): void
     {
         if ($personalDataAccessRequest->getType() === PersonalDataAccessRequest::TYPE_DISPLAY) {
-            $mailTemplate = $this->mailTemplateFacade->get(
+            $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesBody(
                 MailTemplate::PERSONAL_DATA_ACCESS_NAME,
                 $personalDataAccessRequest->getDomainId(),
             );
         } else {
-            $mailTemplate = $this->mailTemplateFacade->get(
+            $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesBody(
                 MailTemplate::PERSONAL_DATA_EXPORT_NAME,
                 $personalDataAccessRequest->getDomainId(),
             );

shopsys/framework

src/Model/Administrator/Mail/TwoFactorAuthenticationMailFacade.php
@@ -32,7 +32,7 @@ class TwoFactorAuthenticationMailFacade implements AuthCodeMailerInterface
      */
     public function sendAuthCode(TwoFactorInterface $administrator): void
     {
-        $mailTemplate = $this->mailTemplateFacade->get(
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesJsBody(
             TwoFactorAuthenticationMail::TWO_FACTOR_AUTHENTICATION_CODE,
             $this->domain->getId(),
         );

src/Model/Complaint/Mail/ComplaintMailFacade.php
@@ -50,7 +50,7 @@ class ComplaintMailFacade
     {
         $templateName = ComplaintMail::getMailTemplateNameByStatus($complaintStatus);
 
-        return $this->mailTemplateFacade->get($templateName, $domainId);
+        return $this->mailTemplateFacade->getWrappedWithGrapesJsBody($templateName, $domainId);
     }
 
     /**

src/Model/Customer/Mail/CustomerMailFacade.php
@@ -34,7 +34,7 @@ class CustomerMailFacade
      */
     public function sendRegistrationMail(CustomerUser $customerUser): void
     {
-        $mailTemplate = $this->mailTemplateFacade->get(
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesJsBody(
             MailTemplate::REGISTRATION_CONFIRM_NAME,
             $customerUser->getDomainId(),
         );
@@ -46,7 +46,7 @@ class CustomerMailFacade
      */
     public function sendActivationMail(CustomerUser $customerUser): void
     {
-        $mailTemplate = $this->mailTemplateFacade->get(CustomerActivationMail::CUSTOMER_ACTIVATION_NAME, $customerUser->getDomainId());
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesJsBody(CustomerActivationMail::CUSTOMER_ACTIVATION_NAME, $customerUser->getDomainId());
         $this->sendActivationMailTemplate($mailTemplate, $customerUser);
     }
 

src/Model/Inquiry/Mail/InquiryMailFacade.php
@@ -32,10 +32,10 @@ class InquiryMailFacade
      */
     public function sendMail(Inquiry $inquiry): void
     {
-        $mailTemplate = $this->mailTemplateFacade->get(InquiryMail::ADMIN_MAIL_TEMPLATE_NAME, $inquiry->getDomainId());
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesJsBody(InquiryMail::ADMIN_MAIL_TEMPLATE_NAME, $inquiry->getDomainId());
         $this->sendMailTemplate($mailTemplate, $inquiry);
 
-        $mailTemplate = $this->mailTemplateFacade->get(InquiryMail::CUSTOMER_MAIL_TEMPLATE_NAME, $inquiry->getDomainId());
+        $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesJsBody(InquiryMail::CUSTOMER_MAIL_TEMPLATE_NAME, $inquiry->getDomainId());
         $this->sendMailTemplate($mailTemplate, $inquiry);
     }
 

src/Model/Order/Mail/OrderMailFacade.php
@@ -50,7 +50,7 @@ class OrderMailFacade
     {
         $templateName = OrderMail::getMailTemplateNameByStatus($orderStatus);
 
-        return $this->mailTemplateFacade->get($templateName, $domainId);
+        return $this->mailTemplateFacade->getWrappedWithGrapesJsBody($templateName, $domainId);
     }
 
     /**

src/Model/PersonalData/Mail/PersonalDataAccessMailFacade.php
@@ -35,12 +35,12 @@ class PersonalDataAccessMailFacade
     public function sendMail(PersonalDataAccessRequest $personalDataAccessRequest): void
     {
         if ($personalDataAccessRequest->getType() === PersonalDataAccessRequest::TYPE_DISPLAY) {
-            $mailTemplate = $this->mailTemplateFacade->get(
+            $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesJsBody(
                 MailTemplate::PERSONAL_DATA_ACCESS_NAME,
                 $personalDataAccessRequest->getDomainId(),
             );
         } else {
-            $mailTemplate = $this->mailTemplateFacade->get(
+            $mailTemplate = $this->mailTemplateFacade->getWrappedWithGrapesJsBody(
                 MailTemplate::PERSONAL_DATA_EXPORT_NAME,
                 $personalDataAccessRequest->getDomainId(),
             );
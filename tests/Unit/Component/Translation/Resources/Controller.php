89ccd82467f782047f8073dac6192e46a2f0081c
Jan Górecki <info@henzigo.cz>
2024-12-21 16:26:18 +0100

    [framework] trans function now support named arguments (#3682)

--- tests/Unit/Component/Translation/Resources/Controller.php
+++ tests/Unit/Component/Translation/Resources/Controller.php
@@ -26,6 +26,15 @@ class Controller extends AbstractController
         t('t test');
         t('t test with domain', [], 'testDomain');
 
+        $locale = 'en';
+
+        t('my %adjective% string', ['%adjective%' => 'awesome'], locale: $locale);
+        t('my string with domain only', domain: 'another-translation-domain');
+        t('my %adjective% string with domain', ['%adjective%' => 'awesome'], domain: 'another-translation-domain');
+        t('my %adjective% string with named locale', ['%adjective%' => 'awesome'], 'someDomain', locale: $locale);
+        t('my %adjective% string with unsorted arguments', ['%adjective%' => 'awesome'], locale: $locale, domain: 'unsortedDomain');
+        t('my %adjective% string with null domain', ['%adjective%' => 'awesome'], null, $locale);
+
         /** @Ignore */
         t('ignored');
         /** @Ignore */

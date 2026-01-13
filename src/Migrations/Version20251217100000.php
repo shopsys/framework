014b58eba2634394d752723f54718ad5f44c26b1
Rostislav Vitek <rostislav.vitek@shopsys.com>
2026-01-13 15:44:58 +0100

    move a DB migration from project-base to the framework package

---
+++ src/Migrations/Version20251217100000.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Override;
+use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20251217100000 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    #[Override]
+    public function up(Schema $schema): void
+    {
+        if ($this->isAppMigrationNotInstalledRemoveIfExists('Version20251217100000')) {
+            $this->sql('ALTER TABLE slider_items ADD route_name VARCHAR(255) DEFAULT NULL');
+        }
+    }
+}

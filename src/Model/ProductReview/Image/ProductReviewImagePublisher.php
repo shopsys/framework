shopsys/shopsys

approved review photos are now published as public static files
- publication is a physical copy of the private customer uploaded file into
  the content images directory, so the photos are served by the standard
  static pipeline with resizing, WebP and CDN without any web server changes
- rejecting a photo or unapproving the review deletes the copy, so the
  public URL stops working immediately
- the inverse side of the review-image association is now kept in sync,
  so the publisher and the mutation response see the images without
  reloading the review

packages/framework/src/Model/ProductReview/Image/ProductReviewImagePublisher.php
@@ -1,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\ProductReview\Image;
+
+use League\Flysystem\FilesystemOperator;
+use League\Flysystem\Visibility;
+use Shopsys\FrameworkBundle\Component\Cdn\CdnFacade;
+use Shopsys\FrameworkBundle\Component\CustomerUploadedFile\CustomerUploadedFile;
+use Shopsys\FrameworkBundle\Component\CustomerUploadedFile\CustomerUploadedFileFacade;
+use Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig;
+use Shopsys\FrameworkBundle\Model\ProductReview\ProductReview;
+use Shopsys\FrameworkBundle\Model\ProductReview\ProductReviewStatusEnum;
+
+/**
+ * Publication of a review photo is a physical copy of the private customer uploaded file
+ * into the public content images directory, where it is served as a static file
+ * with the standard resizing pipeline (nginx image location, imgproxy/CDN, WebP).
+ * Unpublishing deletes the copy, so a rejected photo stops being served immediately.
+ */
+class ProductReviewImagePublisher
+{
+    protected const string PUBLIC_IMAGES_SUBDIR = 'productReviewImage';
+
+    public function __construct(
+        protected readonly FilesystemOperator $filesystem,
+        protected readonly CustomerUploadedFileFacade $customerUploadedFileFacade,
+        protected readonly CdnFacade $cdnFacade,
+        protected readonly string $imageDir,
+        protected readonly string $imageUrlPrefix,
+    ) {
+    }
+
+    /**
+     * Aligns the public copies of the photos with the moderation state - a photo is public
+     * if and only if its review is approved and the photo itself is not rejected
+     */
+    public function reconcile(ProductReview $productReview): void
+    {
+        $isReviewPublic = $productReview->getStatus() === ProductReviewStatusEnum::STATUS_APPROVED;
+
+        foreach ($productReview->getImages() as $productReviewImage) {
+            if ($isReviewPublic && !$productReviewImage->isRejected()) {
+                $this->publish($productReviewImage);
+            } else {
+                $this->unpublish($productReviewImage);
+            }
+        }
+    }
+
+    public function getPublicUrl(
+        DomainConfig $domainConfig,
+        ProductReviewImage $productReviewImage,
+        CustomerUploadedFile $customerUploadedFile,
+    ): string {
+        return $this->cdnFacade->resolveDomainUrlForAssets($domainConfig)
+            . $this->imageUrlPrefix
+            . static::PUBLIC_IMAGES_SUBDIR . '/'
+            . $this->getPublicFilename($productReviewImage, $customerUploadedFile);
+    }
+
+    protected function publish(ProductReviewImage $productReviewImage): void
+    {
+        foreach ($this->customerUploadedFileFacade->getUploadedFilesByEntity($productReviewImage) as $customerUploadedFile) {
+            $stream = $this->filesystem->readStream(
+                $this->customerUploadedFileFacade->getAbsoluteUploadedFileFilepath($customerUploadedFile),
+            );
+
+            $this->filesystem->writeStream(
+                $this->getPublicFilepath($productReviewImage, $customerUploadedFile),
+                $stream,
+                ['visibility' => Visibility::PUBLIC],
+            );
+
+            if (is_resource($stream)) {
+                fclose($stream);
+            }
+        }
+    }
+
+    public function unpublish(ProductReviewImage $productReviewImage): void
+    {
+        foreach ($this->customerUploadedFileFacade->getUploadedFilesByEntity($productReviewImage) as $customerUploadedFile) {
+            $publicFilepath = $this->getPublicFilepath($productReviewImage, $customerUploadedFile);
+
+            if ($this->filesystem->fileExists($publicFilepath)) {
+                $this->filesystem->delete($publicFilepath);
+            }
+        }
+    }
+
+    protected function getPublicFilepath(
+        ProductReviewImage $productReviewImage,
+        CustomerUploadedFile $customerUploadedFile,
+    ): string {
+        return $this->imageDir
+            . static::PUBLIC_IMAGES_SUBDIR . '/'
+            . $this->getPublicFilename($productReviewImage, $customerUploadedFile);
+    }
+
+    protected function getPublicFilename(
+        ProductReviewImage $productReviewImage,
+        CustomerUploadedFile $customerUploadedFile,
+    ): string {
+        return $productReviewImage->getId() . '.' . $customerUploadedFile->getExtension();
+    }
+}

packages/framework/src/Model/ProductReview/ProductReviewFacade.php
@@ -15,6 +15,7 @@ use Shopsys\FrameworkBundle\Model\Product\Product;
 use Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationDispatcher;
 use Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationPriorityEnum;
 use Shopsys\FrameworkBundle\Model\ProductReview\Image\ProductReviewImageFactory;
+use Shopsys\FrameworkBundle\Model\ProductReview\Image\ProductReviewImagePublisher;
 
 class ProductReviewFacade
 {
@@ -26,6 +27,7 @@ class ProductReviewFacade
         protected readonly EntityLogNoteRegistry $entityLogNoteRegistry,
         protected readonly ProductReviewImageFactory $productReviewImageFactory,
         protected readonly CustomerUploadedFileFacade $customerUploadedFileFacade,
+        protected readonly ProductReviewImagePublisher $productReviewImagePublisher,
     ) {
     }
 
@@ -57,9 +59,9 @@ class ProductReviewFacade
         }
 
         $productReview->edit($productReviewData);
-        $this->editImages($productReview, $productReviewData->images);
-
         $this->em->flush();
+
+        $this->editImages($productReview, $productReviewData->images);
 
         $this->dispatchReviewsExport($productReview);
     }
@@ -102,6 +104,8 @@ class ProductReviewFacade
                 $productReview->getCustomerUser(),
             );
         }
+
+        $this->productReviewImagePublisher->reconcile($productReview);
     }
 
     /**
@@ -122,6 +126,10 @@ class ProductReviewFacade
                 $productReviewImage->edit($imagesDataById[$productReviewImage->getId()]);
             }
         }
+
+        $this->em->flush();
+
+        $this->productReviewImagePublisher->reconcile($productReview);
     }
 
     protected function dispatchReviewsExport(ProductReview $productReview): void

shopsys/framework

src/Model/ProductReview/Image/ProductReviewImagePublisher.php
@@ -1,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Model\ProductReview\Image;
+
+use League\Flysystem\FilesystemOperator;
+use League\Flysystem\Visibility;
+use Shopsys\FrameworkBundle\Component\Cdn\CdnFacade;
+use Shopsys\FrameworkBundle\Component\CustomerUploadedFile\CustomerUploadedFile;
+use Shopsys\FrameworkBundle\Component\CustomerUploadedFile\CustomerUploadedFileFacade;
+use Shopsys\FrameworkBundle\Component\Domain\Config\DomainConfig;
+use Shopsys\FrameworkBundle\Model\ProductReview\ProductReview;
+use Shopsys\FrameworkBundle\Model\ProductReview\ProductReviewStatusEnum;
+
+/**
+ * Publication of a review photo is a physical copy of the private customer uploaded file
+ * into the public content images directory, where it is served as a static file
+ * with the standard resizing pipeline (nginx image location, imgproxy/CDN, WebP).
+ * Unpublishing deletes the copy, so a rejected photo stops being served immediately.
+ */
+class ProductReviewImagePublisher
+{
+    protected const string PUBLIC_IMAGES_SUBDIR = 'productReviewImage';
+
+    public function __construct(
+        protected readonly FilesystemOperator $filesystem,
+        protected readonly CustomerUploadedFileFacade $customerUploadedFileFacade,
+        protected readonly CdnFacade $cdnFacade,
+        protected readonly string $imageDir,
+        protected readonly string $imageUrlPrefix,
+    ) {
+    }
+
+    /**
+     * Aligns the public copies of the photos with the moderation state - a photo is public
+     * if and only if its review is approved and the photo itself is not rejected
+     */
+    public function refreshPublicationStatus(ProductReview $productReview): void
+    {
+        $isReviewPublic = $productReview->getStatus() === ProductReviewStatusEnum::STATUS_APPROVED;
+
+        foreach ($productReview->getImages() as $productReviewImage) {
+            if ($isReviewPublic && !$productReviewImage->isRejected()) {
+                $this->publish($productReviewImage);
+            } else {
+                $this->unpublish($productReviewImage);
+            }
+        }
+    }
+
+    public function getPublicUrl(
+        DomainConfig $domainConfig,
+        ProductReviewImage $productReviewImage,
+        CustomerUploadedFile $customerUploadedFile,
+    ): string {
+        return $this->cdnFacade->resolveDomainUrlForAssets($domainConfig)
+            . $this->imageUrlPrefix
+            . static::PUBLIC_IMAGES_SUBDIR . '/'
+            . $this->getPublicFilename($productReviewImage, $customerUploadedFile);
+    }
+
+    protected function publish(ProductReviewImage $productReviewImage): void
+    {
+        foreach ($this->customerUploadedFileFacade->getUploadedFilesByEntity($productReviewImage) as $customerUploadedFile) {
+            $this->filesystem->copy(
+                $this->customerUploadedFileFacade->getAbsoluteUploadedFileFilepath($customerUploadedFile),
+                $this->getPublicFilepath($productReviewImage, $customerUploadedFile),
+                ['visibility' => Visibility::PUBLIC],
+            );
+        }
+    }
+
+    public function unpublish(ProductReviewImage $productReviewImage): void
+    {
+        foreach ($this->customerUploadedFileFacade->getUploadedFilesByEntity($productReviewImage) as $customerUploadedFile) {
+            $publicFilepath = $this->getPublicFilepath($productReviewImage, $customerUploadedFile);
+
+            if ($this->filesystem->fileExists($publicFilepath)) {
+                $this->filesystem->delete($publicFilepath);
+            }
+        }
+    }
+
+    protected function getPublicFilepath(
+        ProductReviewImage $productReviewImage,
+        CustomerUploadedFile $customerUploadedFile,
+    ): string {
+        return $this->imageDir
+            . static::PUBLIC_IMAGES_SUBDIR . '/'
+            . $this->getPublicFilename($productReviewImage, $customerUploadedFile);
+    }
+
+    protected function getPublicFilename(
+        ProductReviewImage $productReviewImage,
+        CustomerUploadedFile $customerUploadedFile,
+    ): string {
+        return $productReviewImage->getId() . '.' . $customerUploadedFile->getExtension();
+    }
+}

src/Model/ProductReview/ProductReviewFacade.php
@@ -16,6 +16,7 @@ use Shopsys\FrameworkBundle\Model\Product\Product;
 use Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationDispatcher;
 use Shopsys\FrameworkBundle\Model\Product\Recalculation\ProductRecalculationPriorityEnum;
 use Shopsys\FrameworkBundle\Model\ProductReview\Image\ProductReviewImageFactory;
+use Shopsys\FrameworkBundle\Model\ProductReview\Image\ProductReviewImagePublisher;
 
 class ProductReviewFacade
 {
@@ -28,6 +29,7 @@ class ProductReviewFacade
         protected readonly CleanStorefrontCacheFacade $cleanStorefrontCacheFacade,
         protected readonly ProductReviewImageFactory $productReviewImageFactory,
         protected readonly CustomerUploadedFileFacade $customerUploadedFileFacade,
+        protected readonly ProductReviewImagePublisher $productReviewImagePublisher,
     ) {
     }
 
@@ -59,9 +61,9 @@ class ProductReviewFacade
         }
 
         $productReview->edit($productReviewData);
-        $this->editImages($productReview, $productReviewData->images);
-
         $this->em->flush();
+
+        $this->editImages($productReview, $productReviewData->images);
 
         $this->dispatchReviewsExport($productReview);
     }
@@ -104,6 +106,8 @@ class ProductReviewFacade
                 $productReview->getCustomerUser(),
             );
         }
+
+        $this->productReviewImagePublisher->refreshPublicationStatus($productReview);
     }
 
     /**
@@ -124,6 +128,10 @@ class ProductReviewFacade
                 $productReviewImage->edit($imagesDataById[$productReviewImage->getId()]);
             }
         }
+
+        $this->em->flush();
+
+        $this->productReviewImagePublisher->refreshPublicationStatus($productReview);
     }
 
     protected function dispatchReviewsExport(ProductReview $productReview): void